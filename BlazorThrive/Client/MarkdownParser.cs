239f5224434e031f98c9be3cd702253e2cb94b16
harveytriana <harveytriana@hotmail.com>
2021-03-10 23:23:56 -0500

    Add Markdig

---
+++ BlazorThrive/Client/MarkdownParser.cs
@@ -0,0 +1,49 @@
+﻿// ======================================
+//  Blazor Spread. LHTV
+// ======================================
+using BlazorThrive.Shared;
+using Markdig;
+using Markdig.SyntaxHighlighting;
+
+namespace BlazorThrive.Client
+{
+    public static class MarkdownParser
+    {
+        public static string Parse(string markdown)
+        {
+            var pipeline = new MarkdownPipelineBuilder()
+                .UseAdvancedExtensions()
+                .UseSyntaxHighlighting()
+                .Build();
+            var html = Markdown.ToHtml(markdown, pipeline);
+            // html surrogates
+            var sr = new string[] {
+                // img style
+                "data-align=|center|".Quotes(),
+                "class=|img-fluid|".Quotes(),
+                // center img
+                "<img",
+                "<p style=|text-align:center;|><img class=|img-fluid|".Quotes(),
+                // external url to new page
+                "a href=",
+                "a target=|_blank| href=".Quotes()
+            };
+
+            // IMGDOC => IMGURL was replaced by base64
+            Surrogate(ref html, sr[0], sr[1]);
+            Surrogate(ref html, sr[2], sr[3]);
+            Surrogate(ref html, sr[4], sr[5]);
+
+            return html;
+        }
+
+        static void Surrogate(ref string text, string contain, string replace)
+        {
+            if (text.Contains(contain)) {
+                text = text.Replace(contain, replace);
+            }
+        }
+    }
+
+    
+}
---
+++ BlazorThrive/Shared/Extensions.cs
@@ -0,0 +1,34 @@
+﻿// ======================================
+//  Blazor Spread. LHTV
+// ======================================
+namespace BlazorThrive.Shared
+{
+    public static class Extensions
+    {
+        public static string Quotes(this string text)
+        {
+            if (string.IsNullOrEmpty(text) == false && text.IndexOf('|') > 0) {
+                return text.Replace("|", "\"");
+            }
+            return text;
+        }
+
+        public static bool IsEmail(this string text)
+        {
+            try {
+                var addr = new System.Net.Mail.MailAddress(text);
+                return addr.Address == text;
+            }
+            catch {
+                return false;
+            }
+        }
+
+        public static bool Empty(this string text)
+        {
+            return string.IsNullOrEmpty(text);
+        }
+
+    }
+
+}
